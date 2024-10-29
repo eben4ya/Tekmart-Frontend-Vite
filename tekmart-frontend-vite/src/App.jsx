afb638e4c0322c4c8d92e1ca05cad59b4f8bb4ec
eben4ya <benaya.imanuela@gmail.com>
2024-10-29 16:52:32 +0700

    <feat: added title component>

--- tekmart-frontend-vite/src/App.jsx
+++ tekmart-frontend-vite/src/App.jsx
@@ -1,10 +1,10 @@
+import Title from "./components/LandingPage/Title";
+
 function App() {
   return (
-    <>
-      <main className="flex flex-col justify-center items-center text-2xl text-red-500 w-screen h-screen">
-        Tekmart App
-      </main>
-    </>
+    <main className="flex flex-col justify-center items-center w-screen h-screen">
+      <Title />
+    </main>
   );
 }
 
---
+++ tekmart-frontend-vite/src/components/LandingPage/Title.jsx
@@ -0,0 +1,7 @@
+const Title = () => {
+  return (
+    <h1 className="text-2xl text-red-500">Tekmart App</h1>
+  )
+}
+
+export default Title

25d71e9fe91bfc39a764400bc453dcc17be6a071
eben4ya <benaya.imanuela@gmail.com>
2024-11-02 23:39:06 +0700

    <feat: added order page & call title component>

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import Navbar from "./components/AllPage/Navbar";
 import LandingPage from "./components/LandingPage/LandingPage";
+import OrderPage from "./components/OrderPage/OrderPage";
+
 import Test from "./test/Test";
 
 function App() {
   return (
     <Router>
       <Navbar />
-      <main className="flex flex-col justify-center items-center w-screen h-screen">
+      <main className="flex flex-col items-center w-screen h-screen">
         <Routes>
           <Route path="/" element={<LandingPage />} />
           {/* route to /about */}
           {/* <Route path="/about" element={<About />} /> */}
           {/* route to /order */}
-          {/* <Route path="/order" element={<Order />} /> */}
+          <Route path="/order" element={<OrderPage />} />
           {/* route to /products */}
           {/* <Route path="/products" element={<Products />} /> */}
           {/* testing route */}

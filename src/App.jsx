aa0f6a54a1d9b1e441768b2ecabfb9d9e68b6eba
eben4ya <benaya.imanuela@gmail.com>
2024-11-02 22:25:50 +0700

    <feat: added routes>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import Hero from "./components/LandingPage/Hero"
 import Navbar from "./components/LandingPage/Navbar"
+
 function App() {
   
 
   return (
-    <main className="flex flex-col justify-center items-center w-screen h-screen">
-     <Navbar />
-     <Hero />
-    </main>
+    <Router>
+      <Navbar />
+      <main className="flex flex-col justify-center items-center w-screen h-screen">
+        <Routes>
+          <Route path="/" element={<Hero />} />
+          {/* route to /about */}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* route to /order */}
+          {/* <Route path="/order" element={<Order />} /> */}
+          {/* route to /products */}
+          {/* <Route path="/products" element={<Products />} /> */}
+        </Routes>
+      </main>
+    </Router>
   )
 }
 

2c780175845ab835a5d844dbb06c6db7e58c1822
eben4ya <benaya.imanuela@gmail.com>
2024-11-08 14:23:35 +0700

    <fix: adjusted App.jsx>

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import './App.css'
-import Hero from './components/LandingPage/Hero'
-import Navbar from './components/LandingPage/Navbar'
-import OrderHeading from './components/OrderPage/OrderHeading.jsx';
+import Navbar from "./components/AllPage/Navbar";
+import LandingPage from "./components/LandingPage/LandingPage";
+import OrderPage from "./components/OrderPage/OrderPage";
 import Footer from './components/OrderPage/Footer';
 
-function App() {
+// Used for testing
+import Test from "./test/Test"; 
 
+function App() {
   return (
-    <main className="flex flex-col justify-center items-center w-screen h-screen">
-     
-     <Navbar />
-     <OrderHeading />
-     <Hero />
-     <Footer />
-    </main>
-  )
+    <Router>
+      <Navbar />
+      <main className="flex flex-col items-center w-screen h-screen">
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* route to /about */}
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* route to /order */}
+          <Route path="/order" element={<OrderPage />} />
+          {/* route to /products */}
+          {/* <Route path="/products" element={<Products />} /> */}
+          {/* testing route */}
+          <Route path="/test" element={<Test />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App

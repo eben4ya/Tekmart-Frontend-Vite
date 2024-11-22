af406cae652d68072f9501de7c74744f2225a9be
eben4ya <benaya.imanuela@gmail.com>
2024-11-22 16:39:24 +0700

    feat: added icon eye in input password
    toggled untoggled show password

--- src/components/RegisterLogin/Input.jsx
+++ src/components/RegisterLogin/Input.jsx
@@ -1,8 +1,16 @@
 import { RiLockPasswordFill } from "react-icons/ri";
 import { FaUser } from "react-icons/fa";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
+import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const Input = ({ type = "", value = "", onChange = () => {} }) => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
   return (
     <div className="flex flex-row justify-center items-center w-full gap-[2.396vw] text-black">
       {type === "email" ? (
@@ -11,7 +19,7 @@ const Input = ({ type = "", value = "", onChange = () => {} }) => {
         <RiLockPasswordFill className="text-white w-[3.385vw] h-[3.385vw]" />
       )}
       <input
-        type={type === "email" ? "email" : "password"}
+        type={type === "email" ? "email" : showPassword ? "text" : "password"}
         name={type}
         value={value}
         onChange={onChange}
@@ -22,6 +30,18 @@ const Input = ({ type = "", value = "", onChange = () => {} }) => {
           type === "email" ? "bg-yellow" : "bg-white"
         } w-[35.3vw] placeholder-black placeholder-opacity-60 text-[1.4vw] rounded-full px-[1.354vw] py-[0.729vw] focus:outline-none`}
       />
+      {type === "password" && (
+        <div
+          className="absolute right-[3vw] cursor-pointer"
+          onClick={toggleShowPassword}
+        >
+          {showPassword ? (
+            <AiFillEyeInvisible className="text-black w-[2.5vw] h-[2.5vw]" />
+          ) : (
+            <AiFillEye className="text-black w-[2.5vw] h-[2.5vw]" />
+          )}
+        </div>
+      )}
     </div>
   );
 };

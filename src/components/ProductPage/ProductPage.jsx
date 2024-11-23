17900549f1f5aeeb35d914fb8cbe7901a20e4319
krystoliz <109867611+krystoliz@users.noreply.github.com>
2024-11-23 20:29:50 +0700

    <feat: chosen item configuration>

--- src/components/ProductPage/ProductPage.jsx
+++ src/components/ProductPage/ProductPage.jsx
@@ -1,6 +1,7 @@
 const ProductPage = () => {
   return (
     <>
+      <div className="w-full max-h-4 mx-auto px-4 py-8">
       {/* Food Card */}
 
       {/* Food Drinks */}
@@ -8,6 +9,22 @@ const ProductPage = () => {
       {/* Food Medicines */}
 
       {/* Food Stationaries */}
+
+      {/* Selected Items Configuration */}
+      <div className='bg-yellow rounded-lg p-2 '> 
+            <div className='flex justify-between items-center p-4 '>
+              <span className="font-bold font-poppins flex items-center justify-between">2 Items Selected
+              </span>
+              <button
+              /* onClick={handlePlaceOrder} */
+              className='font-poppins flex items-center bg-yellow text-black py-3 rounded-lg font-bold hover:bg-yellow transition-colors'>
+                Proceed to Order
+            </button>
+            </div> 
+            
+            
+          </div>
+      </div>
     </>
   );
 };

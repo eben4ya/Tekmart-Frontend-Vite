21a851fb3f1c75aa1599e5b95a117f49217997fe
eben4ya <benaya.imanuela@gmail.com>
2024-11-24 18:50:51 +0700

    feat: added header in about us page

--- src/components/AboutUsPage/AboutPage.jsx
+++ src/components/AboutUsPage/AboutPage.jsx
@@ -1,8 +1,16 @@
+import Title from "../AllPage/Title";
 import AboutUsContent from "./AboutUsContent";
 
 const AboutPage = () => {
   return (
     <>
+      {/* Title */}
+      <Title
+        bgSrc="/images/aboutUsHeader.svg"
+        title="About Us"
+        subtitle="A little background story of why we are doing this."
+      />
+      {/* Content */}
       <AboutUsContent />
     </>
   );

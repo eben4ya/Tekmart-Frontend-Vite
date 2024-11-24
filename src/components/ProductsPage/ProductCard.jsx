adff0e48c4d1b98864c3f201e4431f5784ca0f4a
eben4ya <benaya.imanuela@gmail.com>
2024-11-24 20:51:17 +0700

    feat: render product in product page
    added render product function to display product based on category

--- src/components/ProductsPage/ProductCard.jsx
+++ src/components/ProductsPage/ProductCard.jsx
@@ -3,17 +3,38 @@ import bengBengUrl from "../../assets/Beng-Beng.png";
 import OrderButton from "./OrderButton";
 import ProductDetail from "./ProductDetail";
 
-const dummyProducts = [
+const dummyFoods = [
   { id: 1, image: bengBengUrl, name: "Beng-Beng", price: "IDR4.000,00" },
-  { id: 2, image: bengBengUrl, name: "Sprite", price: "IDR6.000,00" },
-  { id: 3, image: bengBengUrl, name: "Chitato", price: "IDR12.000,00" },
-  { id: 4, image: bengBengUrl, name: "Oreo", price: "IDR8.000,00" },
-  { id: 5, image: bengBengUrl, name: "Milo", price: "IDR5.000,00" },
-  { id: 6, image: bengBengUrl, name: "Sari Roti", price: "IDR10.000,00" },
-  { id: 7, image: bengBengUrl, name: "Cimory", price: "IDR7.000,00" },
-  { id: 8, image: bengBengUrl, name: "SilverQueen", price: "IDR15.000,00" },
-  { id: 9, image: bengBengUrl, name: "Indomie", price: "IDR2.500,00" },
-  { id: 10, image: bengBengUrl, name: "Nutrisari", price: "IDR1.500,00" },
+
+  { id: 2, image: bengBengUrl, name: "Chitato", price: "IDR12.000,00" },
+  { id: 3, image: bengBengUrl, name: "Oreo", price: "IDR8.000,00" },
+
+  { id: 4, image: bengBengUrl, name: "Sari Roti", price: "IDR10.000,00" },
+
+  { id: 5, image: bengBengUrl, name: "SilverQueen", price: "IDR15.000,00" },
+  { id: 6, image: bengBengUrl, name: "Indomie", price: "IDR2.500,00" },
+];
+
+const dummyDrinks = [
+  { id: 7, image: bengBengUrl, name: "Coca-Cola", price: "IDR7.000,00" },
+  { id: 8, image: bengBengUrl, name: "Pepsi", price: "IDR6.500,00" },
+  { id: 9, image: bengBengUrl, name: "Fanta", price: "IDR6.000,00" },
+  { id: 10, image: bengBengUrl, name: "Sprite", price: "IDR6.000,00" },
+  { id: 11, image: bengBengUrl, name: "Milo", price: "IDR5.000,00" },
+  { id: 12, image: bengBengUrl, name: "Cimory", price: "IDR7.000,00" },
+  { id: 13, image: bengBengUrl, name: "Nutrisari", price: "IDR1.500,00" },
+];
+
+const dummyMedicines = [
+  { id: 14, image: bengBengUrl, name: "Paracetamol", price: "IDR5.000,00" },
+  { id: 15, image: bengBengUrl, name: "Bodrex", price: "IDR4.500,00" },
+  { id: 16, image: bengBengUrl, name: "Mixagrip", price: "IDR6.000,00" },
+];
+
+const dummyStationeries = [
+  { id: 17, image: bengBengUrl, name: "Pensil", price: "IDR2.000,00" },
+  { id: 18, image: bengBengUrl, name: "Penghapus", price: "IDR1.000,00" },
+  { id: 19, image: bengBengUrl, name: "Buku Tulis", price: "IDR5.000,00" },
 ];
 
 const ProductCard = () => {
@@ -68,23 +89,39 @@ const ProductCard = () => {
     closeModal();
   };
 
+  // renderProducts function
+  const renderProducts = (title, products) => (
+    <div className="flex flex-wrap w-screen md:px-6 lg:px-7 xl:px-8 font-poppins">
+      <h1>{title}</h1>
+      {products.map((product) => (
+        <div
+          key={product.id}
+          className={`flex flex-col border rounded-xl items-center md:text-md lg:text-lg xl:text-xl md:w-[210px] lg:w-[211px] xl:w-[213px] md:py-5 lg:py-6 xl:py-7 cursor-pointer m-2 ${
+            cart.some((item) => item.id === product.id) ? "bg-yellow" : ""
+          }`}
+          onClick={() => handleCardClick(product)}
+        >
+          <img src={product.image} alt={product.name} />
+          <h1 className="font-bold">{product.name}</h1>
+          <h1>{product.price}</h1>
+        </div>
+      ))}
+    </div>
+  );
+
   return (
-    <div>
-      <div className="flex flex-wrap w-screen md:px-6 lg:px-7 xl:px-8 font-poppins">
-        {dummyProducts.map((product) => (
-          <div
-            key={product.id}
-            className={`flex flex-col border rounded-xl items-center md:text-md lg:text-lg xl:text-xl md:w-[210px] lg:w-[211px] xl:w-[213px] md:py-5 lg:py-6 xl:py-7 cursor-pointer m-2 ${
-              cart.some((item) => item.id === product.id) ? "bg-yellow" : ""
-            }`}
-            onClick={() => handleCardClick(product)}
-          >
-            <img src={product.image} alt={product.name} />
-            <h1 className="font-bold">{product.name}</h1>
-            <h1>{product.price}</h1>
-          </div>
-        ))}
-      </div>
+    <div className="">
+      {/* Foods Product */}
+      {renderProducts("Foods", dummyFoods)}
+
+      {/* Drinks Product */}
+      {renderProducts("Drinks", dummyDrinks)}
+
+      {/* Medicines Product */}
+      {renderProducts("Medicines", dummyMedicines)}
+
+      {/* Stationeries Product */}
+      {renderProducts("Stationeries", dummyStationeries)}
 
       <OrderButton onClick={() => console.log(cart)} />
 

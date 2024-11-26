0293ea98ea54a1ad980adbc6a3369c9d16d45e14
eben4ya <benaya.imanuela@gmail.com>
2024-11-26 22:01:58 +0700

    feat: successfully get all payment to frontend

--- src/components/AdminPage/PaymentHistory.jsx
+++ src/components/AdminPage/PaymentHistory.jsx
@@ -1,34 +1,11 @@
 import Title from "../AllPage/Title";
-import React, { useState } from "react";
-
-const orders = [
-  {
-    id: 1,
-    orderId: 24583,
-    user: "John Doe",
-    items: [
-      { name: "Product A", quantity: 1, price: 5000 },
-      { name: "Product B", quantity: 1, price: 10000 },
-      { name: "Product C", quantity: 1, price: 12000 },
-    ],
-    paymentMethod: "GOPAY",
-    paymentStatus: "Paid",
-  },
-  {
-    id: 2,
-    orderId: 24584,
-    user: "Jane Doe",
-    items: [
-      { name: "Product D", quantity: 2, price: 15000 },
-      { name: "Product E", quantity: 1, price: 20000 },
-    ],
-    paymentMethod: "GOPAY",
-    paymentStatus: "Not Paid",
-  },
-];
+import { useState, useContext } from "react";
+import { OrderContext } from "../../context/OrderContext";
 
 const PaymentHistory = () => {
+  const { allPayments } = useContext(OrderContext); 
   const [openDropdowns, setOpenDropdowns] = useState([]);
+
 
   const toggleDropdown = (id) => {
     if (openDropdowns.includes(id)) {
@@ -48,26 +25,26 @@ const PaymentHistory = () => {
 
       <div className="p-4 w-full min-h-[34vw]">
         <div>
-          {orders.map((order) => (
-            <div key={order.id} className=" rounded mb-4 p-2">
+          {allPayments.map((payment) => (
+            <div key={payment._id} className=" rounded mb-4 p-2">
               {/* Order Header */}
               <div
                 className="flex items-center flex-row cursor-pointer justify-between bg-white outline rounded-md outline-2 outline-white2 leading-5 h-16"
-                onClick={() => toggleDropdown(order.id)}
+                onClick={() => toggleDropdown(payment._id)}
               >
                 <div className="flex flex-row items-center">
                   <button className="ml-4">
-                    {openDropdowns.includes(order.id) ? "▲" : "▼"}
+                    {openDropdowns.includes(payment._id) ? "▲" : "▼"}
                   </button>
                   <div className="ml-5">
                     <p className="font-semibold">
                       <a className="font-poppins font-medium">
-                        Order {order.orderId}
+                        Order {payment.orderId}
                       </a>{" "}
                       <br />{" "}
-                      <a className="font-poppins font-light">
+                      {/* <a className="font-poppins font-light">
                         User: {order.user}
-                      </a>
+                      </a> */}
                     </p>
                   </div>
                 </div>
@@ -77,10 +54,10 @@ const PaymentHistory = () => {
                   {/* Payment Method and Total */}
                   <p className="text-right">
                     <a className="font-poppins font-light">
-                      {order.paymentMethod}
+                      {payment.paymentMethod}
                     </a>
                     <br />
-                    <a className="font-poppins font-bold">
+                    {/* <a className="font-poppins font-bold">
                       IDR{" "}
                       {order.items
                         .reduce(
@@ -88,25 +65,25 @@ const PaymentHistory = () => {
                           0
                         )
                         .toLocaleString()}
-                    </a>
+                    </a> */}
                   </p>
                   {/* Payment Status */}
                   <div className="">
                     <span
                       className={`font-poppins font-bold px-2 py-1 rounded-md shadow-xl w-full justify-center  ${
-                        order.paymentStatus === "Paid"
+                        payment.status === "Pending"
                           ? "bg-yellow text-black"
                           : "bg-black text-white"
                       }`}
                     >
-                      {order.paymentStatus}
+                      {payment.status}
                     </span>
                   </div>
                 </div>
               </div>
 
               {/* Order Details */}
-              {openDropdowns.includes(order.id) && (
+              {/* {openDropdowns.includes(order.id) && (
                 <div className="mt-4 space-y-4">
                   {order.items.map((item, index) => (
                     <div
@@ -141,7 +118,7 @@ const PaymentHistory = () => {
                     </p>
                   </div>
                 </div>
-              )}
+              )} */}
             </div>
           ))}
         </div>

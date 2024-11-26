b39994f66058c83bdb95e5a692b15333fde0d277
danellazs <danella.zefanya@gmail.com>
2024-11-26 17:06:14 +0700

    Adding redirect link to admins Dashboard

--- src/components/AdminPage/Dashboard.jsx
+++ src/components/AdminPage/Dashboard.jsx
@@ -2,8 +2,23 @@ import Title from "../AllPage/Title";
 import ProductsUrl from "/images/dashboard1.png";
 import OrderUrl from "/images/dashboard2.png";
 import PaymentUrl from "/images/dashboard3.png";
+import { useNavigate } from 'react-router-dom';
 
 const Dashboard = () => {
+
+  const navigate = useNavigate();
+
+  const handleClick = (target) => {
+    // Navigate to different routes based on the target argument
+    if (target === 'products') {
+      navigate('/admin/products');
+    } else if (target === 'order') {
+      navigate('/admin/orders');
+    } else if (target === 'paymentHistory') {
+      navigate('/admin/paymenthistory');
+    }
+  };
+
   return (
     <>
       <Title
@@ -13,6 +28,7 @@ const Dashboard = () => {
       />
       <div className="flex md:flex-row w-screen md:px-6 lg:px-7 xl:px-8 md:text-2xl lg:text-[32px] xl:text-[38px] font-bold font-poppins mx-4 my-10">
         <div
+          onClick={() => handleClick('products')}  
           className="relative group flex justify-center items-center w-1/3 aspect-[11/11.5] bg-cover bg-center rounded-xl"
           style={{ backgroundImage: `url(${ProductsUrl})` }}
         >
@@ -20,6 +36,7 @@ const Dashboard = () => {
           <h1 className="text-white z-10">Products</h1>
         </div>
         <div
+          onClick={() => handleClick('order')} 
           className="relative group flex justify-center items-center w-1/3 md:mx-4 lg:mx-5 xl:mx-6 aspect-[11/11.5] bg-cover bg-center rounded-xl"
           style={{ backgroundImage: `url(${OrderUrl})` }}
         >
@@ -27,6 +44,7 @@ const Dashboard = () => {
           <h1 className="ms-2.5 text-white z-10">Order</h1>
         </div>
         <div
+          onClick={() => handleClick('paymentHistory')} 
           className="relative group flex justify-center items-center w-1/3 aspect-[11/11.5] bg-cover bg-center rounded-xl"
           style={{ backgroundImage: `url(${PaymentUrl})` }}
         >

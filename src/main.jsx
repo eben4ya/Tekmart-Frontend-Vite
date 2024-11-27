0b37beb2d2411c928bcfc2c1616ed40b57fbea1c
eben4ya <benaya.imanuela@gmail.com>
2024-11-27 22:53:25 +0700

    feat: called too small component in main jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,36 @@
-import { StrictMode } from "react";
+import { StrictMode, useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import TooSmall from "./components/AllPage/TooSmall.jsx";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { OrderProvider } from "./context/OrderContext.jsx";
 import { ProductProvider } from "./context/ProductContext.jsx";
 // import { PaymentContext } from "./context/PaymentContext.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <AuthProvider>
-      <OrderProvider>
-        <ProductProvider>
-          <App />
-        </ProductProvider>
-      </OrderProvider>
-    </AuthProvider>
-  </StrictMode>
-);
+const RootComponent = () => {
+  const [isLaptopSize, setIsLaptopSize] = useState(window.innerWidth >= 1280);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsLaptopSize(window.innerWidth >= 1280);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return (
+    <StrictMode>
+      <AuthProvider>
+        <OrderProvider>
+          <ProductProvider>
+            {isLaptopSize ? <App /> : <TooSmall />}
+          </ProductProvider>
+        </OrderProvider>
+      </AuthProvider>
+    </StrictMode>
+  );
+};
+
+createRoot(document.getElementById("root")).render(<RootComponent />);

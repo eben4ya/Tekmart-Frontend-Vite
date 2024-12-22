99be1131851be444c8b81e8b3fbe10e43d9bb967
eben4ya <benaya.imanuela@gmail.com>
2024-12-23 03:08:57 +0700

    fix: error require not defined in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: { 
-    colors:{
-      'yellow': '#FFDE4D',
-      'black': '#343131',
-      'white': '#FFFFFF',
-      'white2': '#E4E4E7',
-      'zinc': '#EEEEF0',
-      'gray':'#D7D6D6',
-      'red':'#FF0000',
-      'blue-link': '#2F80ED',
+  theme: {
+    colors: {
+      yellow: "#FFDE4D",
+      black: "#343131",
+      white: "#FFFFFF",
+      white2: "#E4E4E7",
+      zinc: "#EEEEF0",
+      gray: "#D7D6D6",
+      red: "#FF0000",
+      "blue-link": "#2F80ED",
     },
     extend: {
-      fontFamily:{
-        vinque: ['Vinque', 'serif'],
-        poppins: ['Poppins', 'sans-serif'],
-        inter: ['Inter']
-      }
+      fontFamily: {
+        vinque: ["Vinque", "serif"],
+        poppins: ["Poppins", "sans-serif"],
+        inter: ["Inter"],
+      },
     },
   },
-  plugins: [
-    require('@tailwindcss/aspect-ratio'),
-  ],
-}
-
+  plugins: [import("@tailwindcss/aspect-ratio")],
+};

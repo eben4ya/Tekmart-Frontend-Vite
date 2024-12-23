6ffc8acec00312c4fce38282ba3ef688f7f4ab40
eben4ya <benaya.imanuela@gmail.com>
2024-12-24 00:43:38 +0700

    feat: successfully order cash method in production

--- src/components/OrderPage/OrderPage.jsx
+++ src/components/OrderPage/OrderPage.jsx
@@ -27,8 +27,8 @@ const OrderPage = () => {
   const { isLoggedIn, user } = useContext(AuthContext);
 
   const [customerDetails, setCustomerDetails] = useState({
-    first_name: user.email || "John",
-    email: user.email || "john.doe@example.com",
+    first_name: user.email || "Benaya",
+    email: user.email || "benayaimanuela25@mail.ugm.ac.id",
     phone: "-",
   });
 
@@ -40,11 +40,6 @@ const OrderPage = () => {
     (sum, item) => sum + parseFloat(item.price) * item.quantity,
     0
   );
-
-  const handleFloatingButtonClick = () => {
-    alert("Oke!");
-    // next stepnya
-  };
 
   const handleRemoveItem = (id) => {
     const updatedCart = cart.filter((item) => item.id !== id);
@@ -79,13 +74,49 @@ const OrderPage = () => {
       window.location.href = "/login";
       return;
     } else {
-      // temporary for production
+      // Generate order items
+      const orderItems = cart.map((item) => ({
+        productId: item._id,
+        amount: item.quantity,
+        price: parseFloat(item.price),
+      }));
+
+      // Create order in the database
+      // if payment failed but order created, don't create order again
+      if (failedPayment === false) {
+        try {
+          const response = await fetch(`${import.meta.env.VITE_API_ORDER}`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include", // Include credentials to send cookies automatically
+            body: JSON.stringify({
+              userId: user.id,
+              items: orderItems,
+              totalPrice: totalPrice,
+            }),
+          });
+
+          if (!response.ok) {
+            throw new Error("Failed to create order");
+          } else {
+            const data = await response.json();
+            setOrderId(data._id);
+          }
+        } catch (error) {
+          console.error("Failed to create order", error);
+          alert("Failed to create order");
+          return;
+        }
+      }
+
+      // !temporary for production
       if (selectedPayment === "cash") {
         setShowNotification(true);
         setNotifMessage({
           type: "info",
-          message:
-            "Order placed successfully, here is your unique code: 265901. Please show this code to the cashier",
+          message: `Order placed successfully, please wait & check the order status in basket icon`,
         });
         setCart([]);
         localStorage.setItem("cart", JSON.stringify([]));
@@ -102,114 +133,73 @@ const OrderPage = () => {
         // redirect to list order page
         // window.location.href = `/order/${orderId}`;
       }
-      // development
 
-      // Generate order items
-      //   const orderItems = cart.map((item) => ({
-      //     productId: item._id,
-      //     amount: item.quantity,
-      //     price: parseFloat(item.price),
-      //   }));
-
-      //   // Create order in the database
-      //   // if payment failed but order created, don't create order again
-      //   if (failedPayment === false) {
-      //     try {
-      //       const response = await fetch(`${import.meta.env.VITE_API_ORDER}`, {
-      //         method: "POST",
-      //         headers: {
-      //           "Content-Type": "application/json",
-      //         },
-      //         credentials: "include", // Include credentials to send cookies automatically
-      //         body: JSON.stringify({
-      //           userId: user.id,
-      //           items: orderItems,
-      //           totalPrice: totalPrice,
-      //         }),
-      //       });
-
-      //       if (!response.ok) {
-      //         throw new Error("Failed to create order");
-      //       } else {
-      //         const data = await response.json();
-      //         setOrderId(data._id);
-      //       }
-      //     } catch (error) {
-      //       console.error("Failed to create order", error);
-      //       alert("Failed to create order");
-      //       return;
-      //     }
-      //   }
-
-      //   // if user logged in, place order
-      //   if (selectedPayment === "cash") {
-      //     setShowNotification(true);
-      //     setNotifMessage({
-      //       type: "info",
-      //       message: "Order placed successfully",
+      // !development
+      // if user logged in, place order
+      // if (selectedPayment === "cash") {
+      //   setShowNotification(true);
+      //   setNotifMessage({
+      //     type: "info",
+      //     message: "Order placed successfully",
+      //   });
+      //   setCart([]);
+      //   localStorage.setItem("cart", JSON.stringify([]));
+      //   // redirect to list order page
+      //   window.location.href = `/order/${orderId}`;
+      // } else {
+      //   try {
+      //     const response = await fetch(`${import.meta.env.VITE_API_PAYMENT}`, {
+      //       method: "POST",
+      //       headers: {
+      //         "Content-Type": "application/json",
+      //       },
+      //       body: JSON.stringify({
+      //         orderId: orderId,
+      //         totalPrice: totalPrice,
+      //         customerDetails,
+      //       }),
+      //       credentials: "include",
       //     });
-      //     setCart([]);
-      //     localStorage.setItem("cart", JSON.stringify([]));
-      //     // redirect to list order page
-      //     window.location.href = `/order/${orderId}`;
-      //   } else {
-      //     try {
-      //       const response = await fetch(
-      //         `${import.meta.env.VITE_API_PAYMENT}`,
-      //         {
-      //           method: "POST",
-      //           headers: {
-      //             "Content-Type": "application/json",
-      //           },
-      //           body: JSON.stringify({
-      //             orderId: orderId,
-      //             totalPrice: totalPrice,
-      //             customerDetails,
-      //           }),
-      //           credentials: "include",
-      //         }
-      //       );
 
-      //       if (!response.ok) {
-      //         throw new Error("Failed to initiate payment");
-      //       }
-
-      //       const data = await response.json();
-      //       const { token } = data;
-      //       console.log("Payment Token: " + token);
-
-      //       // !DONT FORGET
-      //       setSnapShow(true);
-
-      //       snapEmbed(token, "snap-container", {
-      //         onSuccess: (result) => {
-      //           alert("Payment Success: " + JSON.stringify(result));
-      //           // give navigation to the next page
-      //           setSnapShow(false);
-      //           window.location.href = `/order/${orderId}`;
-      //         },
-      //         onPending: (result) => {
-      //           alert("Payment Pending: " + JSON.stringify(result));
-      //           setSnapShow(false);
-      //         },
-      //         onError: (result) => {
-      //           alert("Payment Failed: " + JSON.stringify(result));
-      //           setSnapShow(false);
-      //         },
-      //         onClose: () => {
-      //           alert("Payment popup closed");
-      //           setSnapShow(false);
-      //         },
-      //       });
-
-      //       setCart([]);
-      //       setFailedPayment(false);
-      //       localStorage.setItem("cart", JSON.stringify([]));
-      //     } catch (error) {
-      //       console.error("Failed to initiate payment", error);
-      //       // alert("Failed to initiate payment");
-      //       setFailedPayment(true);
+      //     if (!response.ok) {
+      //       throw new Error("Failed to initiate payment");
       //     }
+
+      //     const data = await response.json();
+      //     const { token } = data;
+      //     console.log("Payment Token: " + token);
+
+      //     // !DONT FORGET
+      //     setSnapShow(true);
+
+      //     snapEmbed(token, "snap-container", {
+      //       onSuccess: (result) => {
+      //         alert("Payment Success: " + JSON.stringify(result));
+      //         // give navigation to the next page
+      //         setSnapShow(false);
+      //         window.location.href = `/order/${orderId}`;
+      //       },
+      //       onPending: (result) => {
+      //         alert("Payment Pending: " + JSON.stringify(result));
+      //         setSnapShow(false);
+      //       },
+      //       onError: (result) => {
+      //         alert("Payment Failed: " + JSON.stringify(result));
+      //         setSnapShow(false);
+      //       },
+      //       onClose: () => {
+      //         alert("Payment popup closed");
+      //         setSnapShow(false);
+      //       },
+      //     });
+
+      //     setCart([]);
+      //     setFailedPayment(false);
+      //     localStorage.setItem("cart", JSON.stringify([]));
+      //   } catch (error) {
+      //     console.error("Failed to initiate payment", error);
+      //     // alert("Failed to initiate payment");
+      //     setFailedPayment(true);
       //   }
       // }
     }
